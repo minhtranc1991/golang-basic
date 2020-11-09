fc307152eedadcc3388946790a776e1fbf03de25
Drovenger <64911983+Drovenger@users.noreply.github.com>
2020-11-09 15:40:28 +0700

    variables_3

--- variables_3.go
+++ variables_3.go
@@ -2,10 +2,17 @@ package main
 
 import "fmt"
 
+var (
+	a = 5
+	b = 10
+	c = 15
+)
+
 func main() {
 	var x string
 	x = "first"
 	fmt.Println(x)
 	x = "second"
 	fmt.Println(x)
+	fmt.Println(a, ",", b, ",", c)
 }
