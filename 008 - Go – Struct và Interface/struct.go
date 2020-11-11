5f09c10e2e491cd97c043fcb6efc7bb3fe76f28b
Drovenger <64911983+Drovenger@users.noreply.github.com>
2020-11-11 11:29:05 +0700

    refactor

--- 008 - Go – Struct và Interface/struct.go
+++ 008 - Go – Struct và Interface/struct.go
@@ -41,3 +41,9 @@ func (r *Rectangle) area() float64 {
 	w := distance(r.x1, r.y1, r.x2, r.y1)
 	return l * w
 }
+
+func distance(x1, y1, x2, y2 float64) float64 {
+	a := x2 - x1
+	b := y2 - y1
+	return math.Sqrt(a*a + b*b)
+}
